3c322fec07d8f63351f93b666601f43c7e5af31a
dingdongliang <Ks10Kslhk>
2018-08-23 11:16:27 +0800

    添加后台管理界面、修改MarkDown编辑器图片上传功能完成 编辑器滚动条无效尚未解决

--- src/main/resources/static/js/paper-dashboard.js
+++ src/main/resources/static/js/paper-dashboard.js
@@ -1,19 +1,4 @@
-/*!
 
- =========================================================
- * Paper Dashboard 2 - v2.0.0
- =========================================================
-
- * Product Page: https://www.creative-tim.com/product/paper-dashboard-2
- * Copyright 2018 Creative Tim (/)
-
- * Designed by www.invisionapp.com Coded by www.creative-tim.com
-
- =========================================================
-
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
- */
 
 (function() {
   isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
